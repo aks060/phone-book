1599e419223c1c6fe57af7f5968bb0d05b3e27de
Akash Kumar Sharma <akashhdr@gmail.com>
2020-05-29 03:02:58 +0530

    setup

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('name', 'PhoneBook');
 #database config
 
 define('db_host', 'localhost');
-define('db_user', 'phone');
+define('db_user', 'root');
 define('db_pass', 'DB@123');
 define('db_name', 'phone');
 ?>
--- dockerfile
+++
@@ -1,3 +0,0 @@
-FROM httpd
-COPY ./httpd.conf /usr/local/apache2/conf/httpd.conf
-RUN apt update; apt install php -y
